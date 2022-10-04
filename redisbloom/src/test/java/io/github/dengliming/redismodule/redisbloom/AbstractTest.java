4fed138ef99be9ee56708a2e0311479697db1c7e
dengliming <liming.d.pro@gmail.com>
2022-10-04 20:34:03 +0800

    Fix ci

--- redisbloom/src/test/java/io/github/dengliming/redismodule/redisbloom/AbstractTest.java
+++ redisbloom/src/test/java/io/github/dengliming/redismodule/redisbloom/AbstractTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 dengliming.
+ * Copyright 2020-2022 dengliming.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,7 +32,8 @@ public abstract class AbstractTest {
     @BeforeEach
     public void init() {
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://" + TestSettings.host() + ":" + TestSettings.port());
+        config.useSingleServer().setAddress("redis://" + TestSettings.host() + ":" + System.getProperty("REDISBLOOM_PORT"
+                , TestSettings.port() + ""));
         redisBloomClient = new RedisBloomClient(config);
         redisBloomClient.flushall();
     }
